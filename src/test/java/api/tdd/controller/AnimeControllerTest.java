5a8efb6524b35e39d6cbedbd2b965c08d58eeea7
Milene Danelli <mi.danelli@outlook.com>
2021-09-06 15:59:58 -0300

    Ajustes

--- src/test/java/api/tdd/controller/AnimeControllerTest.java
+++ src/test/java/api/tdd/controller/AnimeControllerTest.java
@@ -2,32 +2,20 @@ package api.tdd.controller;
 
 import api.tdd.controllers.AnimeController;
 import api.tdd.models.Anime;
-import api.tdd.repositories.AnimeRepository;
 import api.tdd.services.AnimeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
-import io.restassured.mapper.ObjectMapperDeserializationContext;
-import io.restassured.mapper.ObjectMapperSerializationContext;
-import net.minidev.json.JSONUtil;
-import org.h2.util.geometry.GeoJsonUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -65,21 +53,6 @@ public class AnimeControllerTest {
                 .when().get("/animes/{id}", 1L) //quando chegar uma requisição get no /filmes
                 .then().statusCode(HttpStatus.OK.value()); //então status 200
     }
-
-    @Test
-    public void deveRetornarNaoEncontrado_QuandoBuscarAnime() throws Exception {
-//        when(this.animeService.BuscarAnimePorId(1L)).thenReturn(new Anime(1L, "Mirai Nikki", "Sem Descrição"));
-
-        mockMvc.perform(get("/animes/" + 5)
-        ).andExpect(status().isNotFound());
-    }
-//
-//    @Test
-//    public void deveRetornarBadRequest_QuandoBuscarAnime() {
-//        RestAssuredMockMvc.given().accept(ContentType.JSON).when().get("/animes/{id}", -1L).then().statusCode(HttpStatus.BAD_REQUEST.value());
-//
-//        verify(this.animeService, never()).BuscarAnimePorId(-1L);
-//    }
 
     @Test
     public void deveRetornarSucesso_AoSalvarAnime() throws Exception {
--- src/test/java/api/tdd/service/AnimeServiceTest.java
+++ src/test/java/api/tdd/service/AnimeServiceTest.java
@@ -8,10 +8,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-
 import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
