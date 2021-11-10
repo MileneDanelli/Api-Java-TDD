55dad1631b77f2af1a7999a8daefbe5f146726f7
Milene Danelli <milene.danelli@friellaita.local>
2021-11-10 12:59:57 -0300

    ajustes no ContatoServiceTest.

--- src/test/java/api/tdd/service/ContatoServiceTest.java
+++ src/test/java/api/tdd/service/ContatoServiceTest.java
@@ -8,6 +8,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,46 +19,42 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class ContatoServiceTest {
 
-//    @Mock
-//    private ContatoRepository contatoRepository;
-//
-//    private ContatoService contatoService;
-//
-//    @BeforeEach
-//    void setUp() {
-//        contatoService = new ContatoService(contatoRepository);
-//    }
-//
-//    @Test
-//    public void deveRetornarSucesso_QuandoCadastrarContato() {
-//        Contato contato = Contato.builder()
-//                .id(1L)
-//                .nome("mi")
-//                .email("mi@gmail.com")
-//                .telefone("712371237")
-//                .build();
-//
-//        Mockito.doReturn(contato).when(contatoRepository).save(any(Contato.class));
-//
-//        var result = contatoService.CadastrarContato(contato);
-//
-//        Assertions.assertThat(result)
-//                .isNotNull();
-//        Assertions.assertThat(result.getEmail())
-//                .isEqualTo(contato.getEmail());
-//    }
-//
-//    @Test
-//    public void retornaExcessaoSeEmailJaExiste() {
-//        Contato contato = Contato.builder()
-//                .id(1L)
-//                .nome("mi")
-//                .email("mi@gmail.com")
-//                .telefone("712371237")
-//                .build();
-//
-//        Mockito.when(contatoRepository.findContatoByEmail(any())).thenReturn(Optional.of(contato));
-//
-//        Assertions.assertThatThrownBy(() -> contatoService.validacaoEmail("mi@gmail.com")).isInstanceOf(ExcessaoJaExistente.class);
-//    }
+    @Mock
+    private ContatoRepository contatoRepository;
+
+    @InjectMocks
+    private ContatoService contatoService;
+
+    @Test
+    public void deveRetornarSucesso_QuandoCadastrarContato() {
+        Contato contato = Contato.builder()
+                .id(1L)
+                .nome("mi")
+                .email("mi@gmail.com")
+                .telefone("712371237")
+                .build();
+
+        Mockito.doReturn(contato).when(contatoRepository).save(any(Contato.class));
+
+        var result = contatoService.CadastrarContato(contato);
+
+        Assertions.assertThat(result)
+                .isNotNull();
+        Assertions.assertThat(result.getEmail())
+                .isEqualTo(contato.getEmail());
+    }
+
+    @Test
+    public void retornaExcessaoSeEmailJaExiste() {
+        Contato contato = Contato.builder()
+                .id(1L)
+                .nome("mi")
+                .email("mi@gmail.com")
+                .telefone("712371237")
+                .build();
+
+        Mockito.when(contatoRepository.findContatoByEmail(any())).thenReturn(Optional.of(contato));
+
+        Assertions.assertThatThrownBy(() -> contatoService.validacaoEmail("mi@gmail.com")).isInstanceOf(ExcessaoJaExistente.class);
+    }
 }
